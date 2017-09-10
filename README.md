d3935f7b860c1bcffb2a922328f689da35533f0d
System <none>
2017-09-10 04:21:16 +0000

    added moderately intelligible comments

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 Tweet Counter
-=========================
+=============
 by Mark Smith
 -------------
 
-Returns the total number of tweets (including retweets) by a user in a specified span of time.
+Returns the total number of tweets (including retweets) by a user in a specified span of time. 
 
 See it live on [Glitch](https://vine-kitty.glitch.me/).
+
+Contribute on [GitHub](https://github.com/mark20044/tweet-counter).
 
 **Glitch** is the friendly commmunity where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
 
--- countTweets.js
+++ countTweets.js
@@ -6,6 +6,7 @@ module.exports = function (user, start, end, callback) {
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
+  
   // DST check courtesy of https://stackoverflow.com/questions/11887934/how-to-check-if-the-dst-daylight-saving-time-is-in-effect-and-if-it-is-whats
   Date.prototype.stdTimezoneOffset = function () {
     var jan = new Date(this.getFullYear(), 0, 1);
@@ -19,14 +20,18 @@ module.exports = function (user, start, end, callback) {
   
   var today = new Date();
   var offset = today.dst() ? 16 : 17;
-
+  // thanks stackoverflow, you're the best
+  
+  // max_id is the newest tweet to retrieve, first run is null to get the very newest
   var max_id = null;
   getTweets([], start, user, max_id, t => {
 
     start = new Date(start);
     end = new Date(end);
+    // N = ms per day
     var N = 86400000;
-
+    
+    // Sometimes people forget what "start" and "end" mean
     if (start > end) {
       var ph = start;
       start = end;
@@ -36,11 +41,15 @@ module.exports = function (user, start, end, callback) {
     // convert to noon Eastern 
     start = new Date(start.setUTCHours(offset));
     end = new Date() < new Date(end.setUTCHours(offset)) ? new Date() : new Date(end.setUTCHours(offset));
-
+    
+    // Compute total number of days, rounded to the nearest tenth
     var totaldays = Math.round(10 * (end - start) / N) / 10;
-
-    t = t.length < 1 ? [] : t.filter(x => {
-      var d = typeof x != "undefined" && x.hasOwnProperty("created_at") ? new Date(x.created_at) : new Date();
+    
+    // if tweets were returned, remove any from the array that fall outside the specified time span
+    t = t.length < 1 ? t : t.filter((x, i , a) => {
+      // if the array contains tweet objects, assign d with their created_at date
+      var d = x.hasOwnProperty("created_at") ? new Date(x.created_at) : 0;
+      // TODO: check that x.id is unique
       return (d <= end && d >= start);
     });
 
@@ -60,6 +69,7 @@ module.exports = function (user, start, end, callback) {
   });
 
   function getTweets(t, s, u, m, cb) {
+    // As of 1.1, the statuses/user_timeline always includes retweets regardless of the include_rts value
     var options = {
       screen_name: u,
       count: 200,
@@ -67,20 +77,27 @@ module.exports = function (user, start, end, callback) {
       trim_user: 1,
       include_rts: 1
     };
+    // Only include max_id value on subsequent calls
     if (m != null) options.max_id = m;
-
+    
+    // Call Twitter API
     client.get('statuses/user_timeline', options, (err, twt, res) => {
       if (err) console.error(err);
+      
+      // On first call, t is an empty array
       t = t.concat(twt);
-
+      
+      // Find the oldest of the 200 tweets
       var oldest = t[t.length - 1];
       console.log("Oldest tweet is from: " + oldest.created_at);
 
+      // If the oldest tweet is newer than the start date requested (and we got tweets this run), get another 200 tweets...
       if (new Date(oldest.created_at) > new Date(s) && twt.length > 0) {
         console.log("getting more...");
         getTweets(t, s, u, oldest.id, cb);
 
       } else {
+        // ... otherwise, return the tweets we got
         console.log("Got tweets, executing callback");
         cb(t);
       }
