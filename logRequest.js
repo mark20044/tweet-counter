15179da442670ba6e2587500b1d33a493211bbd1
System <none>
2017-09-11 17:33:33 +0000

    removed unused file, adjusted db close times

--- logRequest.js
+++ logRequest.js
@@ -9,16 +9,18 @@ module.exports = function () {
     MongoClient.connect(url, function (e, db) {
       if (e) {
         console.error('Unable to connect to the mongoDB server. Error:' + e);
+        
+        db.close();
         return "error" + e;
       } else {
   
       var collection = db.collection('request_log');  
       collection.insert({time: time}, (err, data) => {
           if (err) console.error(err);
+        
+          db.close(); 
           return data;
       });
-      
-        db.close();
       
       }
     });
